d55ec448aff907b7ff2d02e58e281ea28e560754
Teguh <teguhpanjaitan@gmail.com>
2020-01-13 19:17:23 +0700

    Post fixture added

---
+++ src/DataFixtures/PostFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Post;
+
+class PostFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        for ($i = 1; $i <= 10; $i++) {
+            $post = new Post();
+            $post->setSlug("slug-" . $i);
+            $post->setTitle("Title " . $i);
+            $post->setContent("The content " . $i);
+
+            if ($i % 2 == 0) {
+                $post->setStatus(false);
+            } else {
+                $post->setStatus(true);
+            }
+
+            $manager->persist($post);
+        }
+        $manager->flush();
+    }
+}
