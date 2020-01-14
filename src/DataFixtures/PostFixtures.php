45c69c08d9894749b06f45d18ecda9490f5f6b71
Teguh <teguhpanjaitan@gmail.com>
2020-01-14 09:52:32 +0700

    update post fixture for the relation update

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -5,9 +5,18 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Post;
+use App\Entity\PostCategory;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PostFixtures extends Fixture
 {
+    private $repository;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->repository = $em->getRepository(PostCategory::class);
+    }
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
@@ -21,6 +30,9 @@ class PostFixtures extends Fixture
             $post->setSeoTitle("SEO title " . $i);
             $post->setSeoDescription("SEO Description " . $i);
             $post->setShortDescription("Short description " . $i);
+
+            $category = $this->repository->findOneBy(['name' => 'Post category name 1']);
+            $post->setCategory($category);
 
             if ($i % 2 == 0) {
                 $post->setStatus(false);
